556cec53959b779ae711178aa5c03d29d1a846b3
Naveen Prakash <191317552+na-v-ee-n@users.noreply.github.com>
2025-11-24 19:28:00 +0530

    first commit with 10 vars

---
+++ OneFile.java
@@ -0,0 +1,24 @@
+public class OneFile {
+    // 10 int variables
+    int num1 = 10;
+    int num2 = 20;
+    int num3 = 30;
+    int num4 = 40;
+    int num5 = 50;
+    int num6 = 60;
+    int num7 = 70;
+    int num8 = 80;
+    int num9 = 90;
+    int num10 = 100;
+
+    // Method to add these 10 variables and print the result
+    public void calculateAndPrintSum() {
+        int sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+        System.out.println("The sum of the 10 numbers is: " + sum);
+    }
+
+    public static void main(String[] args) {
+        OneFile example = new OneFile();
+        example.calculateAndPrintSum();
+    }
+}
